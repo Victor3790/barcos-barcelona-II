3cc2f7fa4b86f7cd63826186d0e021acbeac0fc8
Victor <victor3790@gmail.com>
2020-11-16 15:11:37 -0600

    thumbnails hero images fixed

--- inc/cbt_add_custom_sizes.php
+++ inc/cbt_add_custom_sizes.php
@@ -4,9 +4,17 @@
   */
  function cbt_add_custom_sizes() 
  {
-   add_image_size('cbt_xs', 600, 700, true);
-   add_image_size('cbt_sm', 800, 900, true);
-   add_image_size('cbt_md', 1000, 700, true);
-   add_image_size('cbt_lg', 1300, 850, true);
-   add_image_size('cbt_xl', 1600, 850, true);
+   add_image_size('cbt_xs',         600,  700,  true);
+   add_image_size('cbt_xs_ret',     900,  1000, true);
+   add_image_size('cbt_xs_lan',     700,  600,  true);
+   add_image_size('cbt_xs_lan_ret', 1000, 900,  true);
+   add_image_size('cbt_sm',         800,  900,  true);
+   add_image_size('cbt_sm_ret',     900,  1000, true);
+   add_image_size('cbt_sm_lan',     1000, 900,  true);
+   add_image_size('cbt_sm_lan_ret', 1500, 900,  true);
+   add_image_size('cbt_md',         1000, 700,  true);
+   add_image_size('cbt_lg',         1300, 850,  true);
+   add_image_size('cbt_xl',         1600, 850,  true);
+
+   //add_image_size('cbt_xl',         1600, 850,  true);
  }
--- template-parts/single_yate_styles.php
+++ template-parts/single_yate_styles.php
@@ -1,38 +1,56 @@
 <?php
-/*
-*   Style header to include in the yate single pages.
-*
-*/
-
-global $post;
-
-$bg_large                   = get_the_post_thumbnail_url( $post->ID, 'cbt_xl' );
-$bg_small                   = get_the_post_thumbnail_url( $post->ID, 'cbt_xs');
-$bg_small_retina            = get_the_post_thumbnail_url( $post->ID, 'cbt_md');
-$bg_small_landscape         = get_the_post_thumbnail_url( $post->ID, 'cbt_lg');
-$bg_small_landscape_retina  = get_the_post_thumbnail_url( $post->ID, 'cbt_xl');
+    //Get the header image background in differente sizes.
+    $cbt_bg_xs          = get_the_post_thumbnail_url($post->ID, 'cbt_xs');
+    $cbt_bg_xs_ret      = get_the_post_thumbnail_url($post->ID, 'cbt_xs_ret');
+    $cbt_bg_xs_lan      = get_the_post_thumbnail_url($post->ID, 'cbt_xs_lan');
+    $cbt_bg_xs_lan_ret  = get_the_post_thumbnail_url($post->ID, 'cbt_xs_lan_ret');
+    $cbt_bg_sm          = get_the_post_thumbnail_url($post->ID, 'cbt_sm');
+    $cbt_bg_sm_ret      = get_the_post_thumbnail_url($post->ID, 'cbt_sm_ret');
+    $cbt_bg_sm_lan      = get_the_post_thumbnail_url($post->ID, 'cbt_sm_lan');
+    $cbt_bg_sm_lan_ret  = get_the_post_thumbnail_url($post->ID, 'cbt_sm_lan_ret');
+    $cbt_bg_md          = get_the_post_thumbnail_url($post->ID, 'cbt_md');
+    $cbt_bg_lg          = get_the_post_thumbnail_url($post->ID, 'cbt_lg');
+    $cbt_bg_xl          = get_the_post_thumbnail_url($post->ID, 'cbt_xl');
 ?>
 <style type="text/css">
-    .hero--single
-    {
-        background-image: url('<?php echo $bg_small; ?>');
+
+    .hero--single{
+        background-image: url("<?php echo $cbt_bg_xl; ?>");
+    }
+
+    @media (max-width: 1200px){
+                .hero--single{
+                    background-image: url("<?php echo $cbt_bg_lg; ?>");
+                }
+    }
+
+    @media (max-width: 992px){
+            .hero--single{
+                background-image: url("<?php echo $cbt_bg_md; ?>");
+            }
+    }
+
+    @media (max-width: 768px){
+        .hero--single{
+            background-image: url("<?php echo $cbt_bg_sm; ?>");
+        }
     }
 
     @media
-    only screen and (-webkit-min-device-pixel-ratio: 2)      and (max-width: 576px),
-    only screen and (   min--moz-device-pixel-ratio: 2)      and (max-width: 576px),
-    only screen and (     -o-min-device-pixel-ratio: 2/1)    and (max-width: 576px),
-    only screen and (        min-device-pixel-ratio: 2)      and (max-width: 576px),
-    only screen and (                min-resolution: 192dpi) and (max-width: 576px),
-    only screen and (                min-resolution: 2dppx)  and (max-width: 576px) {
+    only screen and (-webkit-min-device-pixel-ratio: 2)      and (max-width: 768px),
+    only screen and (   min--moz-device-pixel-ratio: 2)      and (max-width: 768px),
+    only screen and (     -o-min-device-pixel-ratio: 2/1)    and (max-width: 768px),
+    only screen and (        min-device-pixel-ratio: 2)      and (max-width: 768px),
+    only screen and (                min-resolution: 192dpi) and (max-width: 768px),
+    only screen and (                min-resolution: 2dppx)  and (max-width: 768px) {
         .hero--single{
-            background-image: url("<?php echo $bg_small_retina; ?>");
+            background-image: url("<?php echo $cbt_bg_sm_ret; ?>");
         }
     }
 
     @media  (max-width: 768px) and (orientation: landscape) {
         .hero--single{
-            background-image: url("<?php echo $bg_small_landscape; ?>");
+            background-image: url("<?php echo $cbt_bg_sm_lan; ?>");
         }
     }
 
@@ -44,13 +62,46 @@ $bg_small_landscape_retina  = get_the_post_thumbnail_url( $post->ID, 'cbt_xl');
     only screen and (                min-resolution: 192dpi) and (max-width: 768px) and (orientation: landscape),
     only screen and (                min-resolution: 2dppx)  and (max-width: 768px) and (orientation: landscape) {
         .hero--single{
-            background-image: url("<?php echo $bg_small_landscape_retina; ?>");
+            background-image: url("<?php echo $cbt_bg_sm_lan_ret; ?>");
         }
     }
 
-    @media (min-width: 768px){
+    @media (max-width: 576px){
         .hero--single{
-            background-image: url("<?php echo $bg_large; ?>");
+            background-image: url("<?php echo $cbt_bg_xs; ?>");
         }
     }
+
+    @media
+    only screen and (-webkit-min-device-pixel-ratio: 2)      and (max-width: 576px),
+    only screen and (   min--moz-device-pixel-ratio: 2)      and (max-width: 576px),
+    only screen and (     -o-min-device-pixel-ratio: 2/1)    and (max-width: 576px),
+    only screen and (        min-device-pixel-ratio: 2)      and (max-width: 576px),
+    only screen and (                min-resolution: 192dpi) and (max-width: 576px),
+    only screen and (                min-resolution: 2dppx)  and (max-width: 576px) {
+        .hero--single{
+            background-image: url("<?php echo $cbt_bg_xs_ret; ?>");
+        }
+    }
+
+    @media  (max-width: 576px) and (orientation: landscape) {
+        .hero--single{
+            background-image: url("<?php echo $cbt_bg_xs_lan; ?>");
+        }
+    }
+
+    @media
+    only screen and (-webkit-min-device-pixel-ratio: 2)      and (max-width: 576px) and (orientation: landscape),
+    only screen and (   min--moz-device-pixel-ratio: 2)      and (max-width: 576px) and (orientation: landscape),
+    only screen and (     -o-min-device-pixel-ratio: 2/1)    and (max-width: 576px) and (orientation: landscape),
+    only screen and (        min-device-pixel-ratio: 2)      and (max-width: 576px) and (orientation: landscape),
+    only screen and (                min-resolution: 192dpi) and (max-width: 576px) and (orientation: landscape),
+    only screen and (                min-resolution: 2dppx)  and (max-width: 576px) and (orientation: landscape) {
+        .hero--single{
+            background-image: url("<?php echo $cbt_bg_xs_lan_ret; ?>");
+        }
+    }
+
+
 </style>
+
