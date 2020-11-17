400c404bf31648c8bd14e9e57f4edde663c47a04
Victor <victor3790@gmail.com>
2020-11-17 08:31:12 -0600

    Data added to the archive page

--- template-parts/result_yacht.php
+++ template-parts/result_yacht.php
@@ -2,8 +2,28 @@
     <div class="card">
         <img class="card__image" src="./imgs/thumbnail2_xs.jpg" alt="">
         <div class="card__info section section--dark">
-            <h3 class="card__title">Nombre</h3>
-            <p class="card__text">Información del barco</p>
+            <a class="card__title card--upper" href="<?php the_permalink(); ?>">
+                <h3>
+                        <?php the_title(); ?>
+                </h3>
+            </a>
+            <p class="card__text card--upper">
+                <?php 
+                    the_field('builder'); 
+                    echo ", ";  
+                    the_field('eslora'); 
+                ?>
+            </p>
+            <p class="card__text">
+                <?php $accommodation = get_field( 'accommodation' ); ?>
+                <?php echo $accommodation['invitados']; ?> 
+                invitados en 
+                <?php echo $accommodation['camarotes']; ?> 
+                camarotes.
+            </p>
+            <p class="card__text">
+                <?php the_field('precio'); ?>
+            </p>
         </div>
     </div>
 </div>
