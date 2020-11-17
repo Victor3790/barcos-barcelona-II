2d8caaa63e6616a5a5d0f0662bc25ae53cf102f9
Victor <victor3790@gmail.com>
2020-11-17 10:35:57 -0600

    Thumbnail images added to archive

--- inc/cbt_add_custom_sizes.php
+++ inc/cbt_add_custom_sizes.php
@@ -4,17 +4,23 @@
   */
  function cbt_add_custom_sizes() 
  {
-   add_image_size('cbt_xs',         600,  700,  true);
-   add_image_size('cbt_xs_ret',     900,  1000, true);
-   add_image_size('cbt_xs_lan',     700,  600,  true);
-   add_image_size('cbt_xs_lan_ret', 1000, 900,  true);
-   add_image_size('cbt_sm',         800,  900,  true);
-   add_image_size('cbt_sm_ret',     900,  1000, true);
-   add_image_size('cbt_sm_lan',     1000, 900,  true);
-   add_image_size('cbt_sm_lan_ret', 1500, 900,  true);
-   add_image_size('cbt_md',         1000, 700,  true);
-   add_image_size('cbt_lg',         1300, 850,  true);
-   add_image_size('cbt_xl',         1600, 850,  true);
+   add_image_size('cbt_xs',             600,  700,  true);
+   add_image_size('cbt_xs_ret',         900,  1000, true);
+   add_image_size('cbt_xs_lan',         700,  600,  true);
+   add_image_size('cbt_xs_lan_ret',     1000, 900,  true);
+   add_image_size('cbt_sm',             800,  900,  true);
+   add_image_size('cbt_sm_ret',         900,  1000, true);
+   add_image_size('cbt_sm_lan',         1000, 900,  true);
+   add_image_size('cbt_sm_lan_ret',     1500, 900,  true);
+   add_image_size('cbt_md',             1000, 700,  true);
+   add_image_size('cbt_lg',             1300, 850,  true);
+   add_image_size('cbt_xl',             1600, 850,  true);
 
-   //add_image_size('cbt_xl',         1600, 850,  true);
+   add_image_size('cbt_xxxs_thumbnail', 400,  200,  true);
+   add_image_size('cbt_xxs_thumbnail',  500,  250,  true);
+   add_image_size('cbt_xs_thumbnail',   600,  300,  true);
+   add_image_size('cbt_sm_thumbnail',   800,  400,  true);
+   add_image_size('cbt_md_thumbnail',   450,  225,  true);
+   add_image_size('cbt_lg_thumbnail',   350,  200,  true);
+   add_image_size('cbt_xl_thumbnail',   500,  300,  true);
  }
--- template-parts/result_yacht.php
+++ template-parts/result_yacht.php
@@ -1,6 +1,36 @@
+<?php
+    $xxxs_thumbnail = get_the_post_thumbnail_url($post->ID, 'cbt_xxxs_thumbnail');
+    $xxs_thumbnail  = get_the_post_thumbnail_url($post->ID, 'cbt_xxs_thumbnail');
+    $xs_thumbnail   = get_the_post_thumbnail_url($post->ID, 'cbt_xs_thumbnail');
+    $sm_thumbnail   = get_the_post_thumbnail_url($post->ID, 'cbt_sm_thumbnail');
+    $md_thumbnail   = get_the_post_thumbnail_url($post->ID, 'cbt_md_thumbnail');
+    $lg_thumbnail   = get_the_post_thumbnail_url($post->ID, 'cbt_lg_thumbnail');
+    $xl_thumbnail   = get_the_post_thumbnail_url($post->ID, 'cbt_xl_thumbnail');
+?>
 <div class="col-12 col-md-6 col-lg-4">
     <div class="card">
-        <img class="card__image" src="./imgs/thumbnail2_xs.jpg" alt="">
+        <a href="<?php the_permalink(); ?>">
+            <div class="card__picture card__picture--center">
+                <img  class="card__picture__img"
+                    src="<?php echo $xl_thumbnail; ?>" 
+                    alt="<?php the_title(); ?>" 
+                    srcset="<?php echo $xxxs_thumbnail; ?>  400w,
+                            <?php echo $xxs_thumbnail;  ?>  500w,
+                            <?php echo $xs_thumbnail;   ?>  600w,
+                            <?php echo $sm_thumbnail;   ?>  800w,
+                            <?php echo $md_thumbnail;   ?>  450w,
+                            <?php echo $lg_thumbnail;   ?>  350w,
+                            <?php echo $xl_thumbnail;   ?>  500w"
+
+                    sizes=" (max-width: 400px)  400px,
+                            (max-width: 480px)  500px,
+                            (max-width: 576px)  600px,
+                            (max-width: 768px)  800px,
+                            (max-width: 992px)  450px,
+                            (max-width: 1200px) 350px,
+                            500px">
+            </div>
+        </a>
         <div class="card__info section section--dark">
             <a class="card__title card--upper" href="<?php the_permalink(); ?>">
                 <h3>
