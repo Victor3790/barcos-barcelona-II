b244eff24c2b9d09bad397022405f6332410a9cc
Victor <victor3790@gmail.com>
2021-02-26 10:21:17 -0600

    Hero full height fixed in yate archive

---
+++ footer-yacht_archive.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Barcos_Barcelona
+ */
+
+?>
+
+	<footer class="footer section section--justified section--white">
+		<div class="footer__copy">
+			<p style="margin:0;">Copyright <?php echo date('Y'); ?> BARCOS BARCELONA S.L.</p>
+		</div>
+		<div class="footer__link">
+			<a style="color:#969696;" href="https://alquilerdeyatesenibiza.com" target="bank">
+				Alquiler de yates en ibiza
+			</a>
+		</div>
+  	</footer>
+</main>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
